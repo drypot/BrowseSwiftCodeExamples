8b916b20940ff5e5e45ebb819b3772c2edf6df32
drypot <drypot@gmail.com>
2026-07-07 22:23:51 +0900

    add OnChangeExample

---
+++ 310 SwiftUI/Form/OnChangeExample.swift
@@ -0,0 +1,33 @@
+//
+//  OnChangeExample.swift
+//  Browse Swift Code Examples
+//
+//  Created by Kyuhyun Park on 7/7/26.
+//
+
+import SwiftUI
+
+struct OnChangeExample: View {
+    @State private var value: Int = 10
+
+    var body: some View {
+        Form {
+            Button("Set 30") {
+                value = 30
+            }
+            Button("Set 40") {
+                value = 40
+            }
+            Button("Set 50") {
+                value = 50
+            }
+        }
+        .onChange(of: value) { _, newValue in
+            print("value: \(newValue)")
+        }
+    }
+}
+
+#Preview {
+    OnChangeExample()
+}
